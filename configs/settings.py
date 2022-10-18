1279b2ead66fb969844e25455779e5b5283559c6
julia <Julia.Mazuryk@gmail..com>
2022-10-18 10:03:35 +0300

    lesso6 - cw

--- configs/settings.py
+++ configs/settings.py
@@ -40,10 +40,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework_simplejwt.token_blacklist',
 
     'apps.users',
     'apps.auto_parks',
     'apps.cars',
+    'core',
 
 ]
 
