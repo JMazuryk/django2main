edd08b8279ed7e13ed86b7baef6f4cde15575d53
julia <Julia.Mazuryk@gmail..com>
2022-10-22 15:46:56 +0300

    lesso6 - hw

---
+++ apps/auth/serializers.py
@@ -0,0 +1,19 @@
+from typing import Type
+
+from django.contrib.auth import get_user_model
+
+from rest_framework import serializers
+
+from apps.users.models import UserModel as User
+
+UserModel: Type[User] = get_user_model()
+
+
+class EmailSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+
+
+class PasswordSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserModel
+        fields = ('password',)
